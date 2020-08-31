b9e1796aa3c7af60df281b79e95f13954e8a8722
Derek <derekluu90@gmail.com>
2020-08-31 17:05:42 -0400

    created users page, user-list and user-item component

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,19 @@
-import React from 'react'
+import React from 'react';
+
+import UsersList from '../components/UsersList';
 
 const Users = () => {
-  return <h2>User works!</h2>
-}
+  const USERS = [
+    {
+      id: 'u1',
+      name: 'Delvv',
+      image:
+        'https://images.unsplash.com/photo-1462275646964-a0e3386b89fa?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80',
+      places: 3,
+    },
+  ];
 
-export default Users
+  return <UsersList items={USERS} />;
+};
+
+export default Users;
