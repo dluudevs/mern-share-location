fd4232ad9a6b9cbedbc783f62071d7b6a960b21c
Derek <dluu@trewknowledge.com>
2021-03-01 20:44:58 -0500

    establish relation between users and places in schema

--- backend/models/place.js
+++ backend/models/place.js
@@ -10,9 +10,10 @@ const placeSchema = new mongoose.Schema({
     lng: { type: Number, required: true }
    },
   address: { type: String, required: true },
-  creator: { type: String, required: true },
+  // set type to accept an id
+  creator: { type: mongoose.Types.ObjectId, required: true, ref: 'User' }, // ref establishes connection with schema (schema with schema, cant ref model)
 })
 
-// using a schema, models define the structure of a document
+// using a schema, models define the structure of a document. A model is a class with which we construct documents
 // first argument is name of schema, will also serve as name of the collection
 module.exports = mongoose.model('Places', placeSchema);
---
+++ backend/models/user.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const uniquevalidator = require('mongoose-unique-validator');
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  email: { type: String, required: true, unique: true }, //unique creates an index for the email, which speeds up the query when the email is requestec
+  password: { type: String, required: true, minlength: 6 },
+  image: { type: String, required: true },
+  // places is an array a user can have more than one place
+  // set type to accept an id
+  places: [{ type: mongoose.Types.ObjectId, required: true, ref: 'Place' }], // ref establishes connection with schema (schema with schema, cant ref model)
+});
+
+// plugin only creates user if the user doesn't exist already
+userSchema.plugin(uniquevalidator);
+
+// Notes are in place.js models
+module.exports = mongoose.model('Users', userSchema);
